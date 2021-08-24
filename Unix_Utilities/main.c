0f90ac63f6bb214593cf996a46546531f3f3a4ca
Jerry <tug25060@temple.edu>
2021-08-24 19:30:51 -0400

    Initial Commit

---
+++ Unix_Utilities/main.c
@@ -0,0 +1,14 @@
+//
+//  main.c
+//  Unix_Utilities
+//
+//  Created by Jerry Maurice on 8/24/21.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
